4d49a00237e60cdd94913649e81cda7e847fe753
dandelionadia <nadia.ridko@gmail.com>
2019-06-27 12:59:23 +0200

    adds logo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,8 @@ function HomePage() {
     return (
         <div className="header">
             <div className="header__logo">
+                <img className="logo" src="https://brightdotacademy.com/wp-content/uploads/2018/03/3.png"></img>
+                <h2><span className="logo__text">Li</span>ogo</h2>
 
             </div>
             <div className="header__menu">
