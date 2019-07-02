c4d4fc5ada0b5e63af7461b993e0720f4bef8b5d
dandelionadia <nadia.ridko@gmail.com>
2019-07-02 15:29:24 +0200

    adds mobile amd md start

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import { Composition } from 'atomic-layout'
+import Layout from 'atomic-layout'
 import { Menu } from '../components/Menu';
 import { HomeInfo } from '../components/HomeInfo';
 import { Logo } from '../atoms/Logo';
+import styled from 'styled-components'
 
-function HomePage() {
-    return (
-        <div className="container-homePage">
-            <div className="header">
-                <Logo />
-                <Menu />
-            </div>
-            <div className="infoPage">
-                <HomeInfo>"It is a long established fact that a reader will be distracted by the readable content of a page."</HomeInfo>
-                <img className="background__img" src="https://cdn.dribbble.com/users/1355613/screenshots/6568571/____41.jpg"></img>
-            </div>
-        </div>
-    )
-}
+Layout.configure({
+    defaultUnit: 'rem',
+})
+
+const areasMobile = `
+    header
+    image
+    info
+`
+
+const areasTablet = `
+    header header
+    info image
+`
+
+const HomePage = () => (
+    <Composition
+        areas={areasMobile}
+        areasMd={areasTablet}
+        gutter={2}
+        padding={2}
+    >
+        {({ Header, Info, Image }) => (
+            <>
+                <Header>
+                    <Logo />
+                    <Menu />
+                </Header>
+                <Image>
+                    <img className="background__img" src="https://cdn.dribbble.com/users/1355613/screenshots/6568571/____41.jpg"></img>
+                </Image>
+                <Info>
+                    <HomeInfo>"It is a long established fact that a reader will be distracted by the readable content of a page."</HomeInfo>
+                </Info>
+            </>
+        )}
+    </Composition>
+)
 
 export { HomePage }
